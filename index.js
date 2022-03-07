521a409c4b14a83ad68e50bcf4347878b9a9f801
Manuel Coloma <zdelp21@gmail.com>
2022-03-07 13:03:26 -0500

    web en mantenimiento

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("whatsapp").addEventListener("click", () => {
+  var a = document.createElement('a');
+  a.target = '_blank';
+  a.href = 'https://wa.link/xhh6nk';
+  a.click();
+});
+
+document.getElementById("facebook").addEventListener("click", () => {
+  var a = document.createElement('a');
+  a.target = '_blank';
+  a.href = 'https://www.facebook.com/PorfinPeru';
+  a.click();
+});
+
